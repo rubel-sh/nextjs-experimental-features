69ba227fd0d65ccdfa2e8a8e498855128f8fbe5e
Rubel Hossain <rubel.hossain0649@gmail.com>
2023-08-29 16:27:38 +0600

    arrange

--- src/pages/virtuoso/index.js
+++ src/pages/virtuoso/index.js
@@ -1,27 +1,41 @@
-import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { Virtuoso } from "react-virtuoso";
+import Image from "next/image";
 
-const index = ({ data }) => {
+const Index = ({ data }) => {
+  const [count, setCount] = useState(0);
+
+  const handleIncreaseClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const renderListItem = (index) => {
+    return (
+      <div className="py-2 text-lg">
+        {data[index].title} {count + index}
+        <Image src={data[index].thumbnailUrl} width={100} height={100} />
+      </div>
+    );
+  };
 
   return (
-    <div className="grid grid-cols-3">
+    <div className="">
+      <button
+        className="flex items-center justify-center"
+        onClick={handleIncreaseClick}
+      >
+        INCREASE
+      </button>
       <Virtuoso
-        style={{ height: "100vh" }}
+        style={{ height: "calc(100vh - 90px)" }}
         totalCount={data.length + 1}
-        itemContent={( index) => (
-          <div className="py-2 text-lg">
-            {data[index].title}
-            <Image src={data[index].thumbnailUrl} width={100} height={100} />
-          </div>
-        )}
+        itemContent={renderListItem}
       />
     </div>
   );
 };
 
 export async function getStaticProps() {
-
   try {
     const url = `https://jsonplaceholder.typicode.com/photos`;
     const res = await fetch(url);
@@ -32,4 +46,4 @@ export async function getStaticProps() {
   }
 }
 
-export default index;
+export default Index;
