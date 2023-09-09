b7575a6fc73a21772ee428dbde73f594ec577bda
Rubel Hossain <rubel.hossain0649@gmail.com>
2023-09-10 01:52:28 +0600

    added- Basic PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+});
+
 const nextConfig = {
     reactStrictMode: true,
     images: {
@@ -9,4 +16,8 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA({
+    ...nextConfig, // Merge your Next.js configuration here
+});
+
+// module.exports = nextConfig;
