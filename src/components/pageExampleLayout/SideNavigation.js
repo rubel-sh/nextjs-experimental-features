3e66efe4bb883b32ab82d5b354e31103c5aaa4f8
Rubel Hossain <rubel.hossain0649@gmail.com>
2023-09-12 02:51:56 +0600

    key

--- src/components/pageExampleLayout/SideNavigation.js
+++ src/components/pageExampleLayout/SideNavigation.js
@@ -24,6 +24,7 @@ const SideNavigation = () => {
         <div className="basis-[350px] min-h-[calc(100vh-120px)] flex flex-col gap-y-2 bg-slate-700 px-2 py-4 font-semibold text-lg text-white">
             {demoLinks.map((link) => (
                 <Link
+                    key={link.name}
                     href={link.link}
                     className="hover:text-slate-300 transition-colors duration-200 py-2 px-3 bg-slate-600 hover:bg-slate-700 rounded-md"
                 >
